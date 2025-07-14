9a7279a8f0f4dde121330f93de1d6cb107ea9d3e
niya <niyabits@gmail.com>
2025-07-14 13:25:48 +0530

    feat: send notifications using magicbell-js

--- app/page.tsx
+++ app/page.tsx
@@ -24,17 +24,41 @@ function PushNotificationManager() {
     return <p>Push notifications are not supported in this browser.</p>;
   }
 
+  const sendNotification = async () => {
+    const res = await fetch("/api/send-notification", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        externalId: "7f4baab5-0c91-44e8-8b58-5ff849535174",
+        message: "Hello from frontend!",
+      }),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className="max-w-md mx-auto mt-8">
       <h2 className="text-2xl font-semibold text-center mb-6">
-        Push Notifications
+        MagicBell Web Push Notifications React Example
       </h2>
-      <WebPushButton
-        renderLabel={({ status, error }) =>
-          error || (status === "success" ? "Unsubscribe" : "Subscribe")
-        }
-        className="bg-[#FFB224] hover:bg-[#FFBF48] transition text-black px-4 py-2 rounded w-full"
-      />
+      <div className="flex items-center gap-2">
+        <WebPushButton
+          renderLabel={({ status, error }) =>
+            error || (status === "success" ? "Unsubscribe" : "Subscribe")
+          }
+          className="bg-[#FFB224] hover:bg-[#FFBF48] transition text-black px-4 py-2 rounded w-full"
+        />
+        <button
+          onClick={sendNotification}
+          className="bg-[#6E56CF] hover:bg-[#836ED6] transition text-white px-4 py-2 rounded w-full"
+        >
+          Send Test Notification
+        </button>
+      </div>
     </div>
   );
 }
